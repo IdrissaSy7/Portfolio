8c0a55de5e9a271372707c7c7a7662bb0101f10f
IdrissaSy7 <112013244+IdrissaSy7@users.noreply.github.com>
2024-08-18 19:45:01 +0200

    modified

--- src/components/SocialNetwork.js
+++ src/components/SocialNetwork.js
@@ -1,20 +1,9 @@
 import React from "react";
-import CV from "../data/CV.pdf";
 
 const SocialNetwork = () => {
   return (
     <div className="social-network">
       <ul className="content">
-        <a
-          href={CV}
-          target={"_blank"}
-          rel="noopener noreferrer"
-          className="hover"
-        >
-          <li>
-            <p>CV</p>
-          </li>
-        </a>
         <a
           href="https://github.com/IdrissaSy7"
           target={"_blank"}
